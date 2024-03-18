1590baeae6ee6aa9a902cee2e7008a7419c94bbd
ekovv <denisovdima2004@yandex.ru>
2024-03-18 17:23:59 +0300

    :tada: config 🔨

--- cmd/creator/main.go
+++ cmd/creator/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"creatorDB/internal/config"
+	"fmt"
+	"log/slog"
+)
+
 func main() {
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
+
+}
+
+func setupLogger(env string) *slog.Logger {
 
 }
