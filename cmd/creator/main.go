8cfd586dc6ab0f6f0e5a9b7ea4a15857c9c84792
ekovv <denisovdima2004@yandex.ru>
2024-03-20 23:00:43 +0300

    :tada: logger ✅

--- cmd/creator/main.go
+++ cmd/creator/main.go
@@ -2,16 +2,32 @@ package main
 
 import (
 	"creatorDB/internal/config"
-	"fmt"
 	"log/slog"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
 )
 
 func main() {
 	cfg := config.MustLoad()
-	fmt.Println(cfg)
-
+	log := setupLogger(cfg.Env)
+	log.Info("starting application")
 }
 
 func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
 
+	switch env {
+	case envLocal:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envDev:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+	case envProd:
+		log = slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
 }
