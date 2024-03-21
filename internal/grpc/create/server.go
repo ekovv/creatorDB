cbc2e82f29129116de7d87ef655e7971f2511eb7
ekovv <denisovdima2004@yandex.ru>
2024-03-21 15:49:05 +0300

    :tada: handler for method✅

--- internal/grpc/create/server.go
+++ internal/grpc/create/server.go
@@ -4,18 +4,42 @@ import (
 	"context"
 	createv1 "github.com/ekovv/protosDB/gen/go/creator"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
+
+type Create interface {
+	CreateDB(ctx context.Context, login, password, DbName, DbType string) (string, error)
+}
 
 type serverAPI struct {
 	createv1.UnimplementedCreatorServer
+	create Create
 }
 
-func Register(gRPC *grpc.Server) {
-	createv1.RegisterCreatorServer(gRPC, &serverAPI{})
+func Register(gRPC *grpc.Server, create Create) {
+	createv1.RegisterCreatorServer(gRPC, &serverAPI{create: create})
 }
 
 func (s *serverAPI) CreateDB(ctx context.Context, req *createv1.CreateDBRequest) (*createv1.CreateDBResponse, error) {
+	if req.GetLogin() == "" {
+		return nil, status.Error((codes.InvalidArgument), "You must provide a login")
+	}
+
+	if req.GetPassword() == "" {
+		return nil, status.Error((codes.InvalidArgument), "You must provide a password")
+	}
+
+	if req.GetDbName() == "" || req.GetDbType() == "" {
+		return nil, status.Error((codes.InvalidArgument), "You must provide a name for database")
+	}
+
+	connectionString, err := s.create.CreateDB(ctx, req.GetLogin(), req.GetPassword(), req.GetDbName(), req.GetDbType())
+	if err != nil {
+		return nil, status.Error(codes.Internal, "internal error creating")
+	}
+
 	return &createv1.CreateDBResponse{
-		ConnectionString: "blablablabla",
+		ConnectionString: connectionString,
 	}, nil
 }
