4615a01a27eb8f1f6d689ab1624dc5b016ef4230
ekovv <denisovdima2004@yandex.ru>
2024-03-21 15:57:30 +0300

    :tada: structure of service ✅

---
+++ internal/domain/models/connections.go
@@ -0,0 +1,8 @@
+package models
+
+type Connection struct {
+	Login    string
+	Password string
+	DbName   string
+	DbType   string
+}
--- internal/services/creator/creator.go
+++ internal/services/creator/creator.go
@@ -1,1 +1,20 @@
 package creator
+
+import (
+	"context"
+	"log/slog"
+)
+
+type Creator struct {
+	log *slog.Logger
+}
+
+func New(log *slog.Logger) *Creator {
+	return &Creator{
+		log: log,
+	}
+}
+
+func (c *Creator) CreateDB(ctx context.Context, login, password, DbaName, DbType string) (string, error) {
+	panic("not implemented")
+}
